50a2d4e0d1b4c73969e191d55f94ba6bd4df284b
James Collins <james@om4.com.au>
2022-08-31 16:35:15 +0800

    Add OpenAI API Client to Laravel Container and Config
    So it can easily be used throughout our app.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\HttpClient\Psr18Client;
+use Tectalic\OpenAi\Authentication;
+use Tectalic\OpenAi\Client;
+use Tectalic\OpenAi\Manager;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(Client::class, function ($app) {
+            // Build the Tectalic OpenAI REST API Client globally.
+            $auth = new Authentication(config('services.openai.token'));
+            $httpClient = new Psr18Client();
+            return Manager::build($httpClient, $auth);
+        });
     }
 
     /**
